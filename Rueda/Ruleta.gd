729383d75f9b00f06eec06f9e32c8eb07bf24c75
arianaishmo <60683323+arianaishmo@users.noreply.github.com>
2021-05-16 15:36:03 -0700

    Gif demostracion

--- Rueda/Ruleta.gd
+++ Rueda/Ruleta.gd
@@ -6,7 +6,8 @@ var rotate
 var t
 var r 
 
-# Called when the node enters the scene tree for the first time.
+# Se inicializan las variables de t (tiempo) y rotate (tetha)
+# La variable r nos sevirá como indicador de que la ruleta tiene que parar.
 func _ready():
 	self.set_process(true)
 	rotate = 0
@@ -17,16 +18,20 @@ func _ready():
 #Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if r == true:
+		# Ecuación de tetha respecto al tiempo expreseada en la práctica
 		rotate = -1 - 0.6 * t + 0.25 * t * t
 		img.rotate(deg2rad(rotate))
+		
+		# Se depliega el tiempo y tetha en unas etiquetas de la pantalla principal
 		$RichTextLabel.text += "\n" + str(t)
 		$RichTextLabel2.text += "\n" + str(rotate)
 	else:
 		rotate = 0
 	
-	
+	# Indicador de que la ruleta debe parar a los 10 segundos (modificable)
 	if t > 10:
 		r = false
-
+	
+	#Acumulador del tiempo
 	t += delta
 	
